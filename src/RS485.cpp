c31cc55d1c04402297b4a84feb1f56b69a335f68
thisdp <30386052+thisdp@users.noreply.github.com>
2024-03-29 09:36:25 +0800

    refactor

--- src/RS485.cpp
+++ src/RS485.cpp
@@ -4,22 +4,107 @@ RS485::RS485(int uart_nr):HardwareSerial(uart_nr){}
 
 RS485::RS485(const HardwareSerial& serial):HardwareSerial(serial){}
 
-void RS485::enableWrite(bool writeEnabled){
-  digitalWrite(pinDE,writeEnabled);
+void RS485::setWriteEnabled(bool enabled) {
+  if(pinDE != -1) digitalWrite(pinDE,enabled);
 }
 
-void RS485::begin(size_t baud, uint32_t config, int8_t rxPin, int8_t txPin, int8_t dePin){
+void RS485::setReadEnabled(bool enabled){
+  if(pinRE != -1) digitalWrite(pinRE,!enabled);
+}
+
+void RS485::setMode(uint8_t mode){
+  switch(mode){
+    case ModeNone:
+      setWriteEnabled(false);
+      setReadEnabled(false);
+    break;
+    case ModeTX:
+      setWriteEnabled(true);
+      setReadEnabled(false);
+    break;
+    case ModeRX:
+      setWriteEnabled(false);
+      setReadEnabled(true);
+    break;
+  }
+}
+
+void RS485::begin(size_t baud, uint32_t config, int8_t rxPin, int8_t txPin, int8_t dePin, int8_t rePin, bool readBack){
   pinDE = dePin;
+  pinRE = rePin;
+  hasReadBack = readBack;
   ((HardwareSerial*) this)->begin(baud,config,rxPin,txPin);
-  pinMode(pinDE,OUTPUT);
-  digitalWrite(pinDE,LOW);
+  setDelay(0,0);
+  if(pinDE != -1){
+    pinMode(pinDE,OUTPUT);
+    digitalWrite(pinDE,LOW);
+  }
+  if(pinRE != -1){
+    pinMode(pinRE,OUTPUT);
+    digitalWrite(pinRE,HIGH);
+  }
+}
+
+void RS485::begin(RS485Config conf){
+  begin(conf.baudrate, conf.config, conf.pinRX, conf.pinTX, conf.pinDE, conf.pinRE, conf.readBack);
+}
+
+void RS485::setDelay(uint32_t start, uint32_t stop){
+  startDelay = start;
+  stopDelay = stop;
 }
 
 void RS485::send(uint8_t *data,uint16_t length){
-  enableWrite(true);
+  beginTransmission();
   write(data,length);
+  endTransmission();
+}
+
+size_t RS485::write(uint8_t c){
+  HardwareSerial::write(c);
+  readBackCountTotal += 1;
+  readBack();
+  return 1;
+}
+
+size_t RS485::write(const uint8_t *buffer, size_t size){
+  HardwareSerial::write(buffer,size);
+  readBackCountTotal += size;
+  readBack();
+  return size;
+}
+
+void RS485::readBack(){
+  if(!hasReadBack || !readBackCountTotal) return;
+  while(available()){
+    read();
+    readBackCount++;
+  }
+}
+
+void RS485::beginTransmission(){
+  setMode(ModeTX);
+  delayMicroseconds(startDelay);
+  readBackCount = 0;
+  readBackCountTotal = 0;
+}
+
+void RS485::endTransmission(){
   flush(true);
-  enableWrite(false);
-  uint8_t removeItems = available();
-  while(removeItems--) read();
+  delayMicroseconds(stopDelay);
+  setMode(ModeRX);
+  if(!hasReadBack || !readBackCountTotal) return;
+  uint32_t ms = millis();
+  while(readBackCountTotal-readBackCount){
+    if(available()){
+      read();
+      readBackCount++;
+    }else if(millis()-ms>=100){
+      Serial.print("[Error] 485 Read Back Failed Read/Total:");
+      Serial.print(readBackCount);
+      Serial.print("/");
+      Serial.println(readBackCountTotal);
+      break;
+    }
+  }
 }
--- src/RS485.h
+++ src/RS485.h
@@ -1,14 +1,43 @@
 #pragma once
 #include "HardwareSerial.h"
 
+class RS485Config{
+public:
+  uint32_t baudrate;
+  uint32_t config;
+  int8_t pinRX;
+  int8_t pinTX;
+  int8_t pinDE;
+  int8_t pinRE;
+  bool readBack;
+};
+
 class RS485 : public HardwareSerial{
 public:
+  constexpr static uint8_t ModeNone = 0x00;
+  constexpr static uint8_t ModeTX = 0x01;
+  constexpr static uint8_t ModeRX = 0x02;
 	RS485(int uart_nr);
   RS485(const HardwareSerial& serial);
-  void begin(size_t baud, uint32_t config = SERIAL_8N1, int8_t rxPin=-1, int8_t txPin=-1, int8_t dePin=-1);
-  void enableWrite(bool writeEnable);
+  void begin(size_t baud, uint32_t config = SERIAL_8N1, int8_t rxPin=-1, int8_t txPin=-1, int8_t dePin=-1, int8_t rePin = -1, bool readBack = false);
+  void begin(RS485Config conf);
+  void setDelay(uint32_t start, uint32_t stop);
+  void setWriteEnabled(bool enabled);
+  void setReadEnabled(bool enabled);
+  void setMode(uint8_t mode);
   void send(uint8_t *data,uint16_t length);
-  int8_t pinDE;
-  int8_t pinTx;
-  int8_t pinRx;
+  void beginTransmission();
+  size_t write(uint8_t);
+  size_t write(const uint8_t *buffer, size_t size);
+  void endTransmission();
+  void readBack();
+  uint32_t readBackCountTotal;
+  uint32_t readBackCount;
+  uint32_t startDelay;
+  uint32_t stopDelay;
+  bool hasReadBack;
+  int8_t pinDE; // Write Enable Pin
+  int8_t pinRE; // Read Enable Pin
+  int8_t pinTx; // TX Pin
+  int8_t pinRx; // RX Pin
 };
